0debd2b67978f550786b3ebb265700a1f8bc3270
Janina-janina123 <mdhusaedislambiplob123@gmail.com>
2024-10-16 22:17:43 +0600

    Update Uptime.js

--- scripts/cmds/Uptime.js
+++ scripts/cmds/Uptime.js
@@ -22,7 +22,7 @@
     const minutes = Math.floor((uptime / 60) % 60);
     const hours = Math.floor((uptime / (60 * 60)) % 24);
     const days = Math.floor(uptime / (60 * 60 * 24));
-    const uptimeString = `╭──𝚂𝙺 𝙽𝙸 𝚂 𝙰𝙽 𝙱𝚘𝚃 ☔︎𝚄𝙿𝚃𝚒𝚖𝚎⏳👈\n├⏳ 𝙷𝙾𝚄𝚁𝚂 ${hours}\n├⏰ 𝙼𝙸𝙽𝚄𝚃𝙴𝚂 ${minutes}\n├⏲ 𝚂𝙴𝙲𝙾𝙽𝙳𝚂 ${seconds}\n╰───────────✰`;
+    const uptimeString = `╭──Ꮇax~ᴿambo~king࿐☂𝚄𝙿𝚃𝚒𝚖𝚎⏳👈\n├⏳ 𝙷𝙾𝚄𝚁𝚂 ${hours}\n├⏰ 𝙼𝙸𝙽𝚄𝚃𝙴𝚂 ${minutes}\n├⏲ 𝚂𝙴𝙲𝙾𝙽𝙳𝚂 ${seconds}\n╰───────────✰`;
     api.sendMessage(`${uptimeString}`, event.threadID);
   }
 }
